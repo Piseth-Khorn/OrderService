9d7ea7b0f394337f0adc1fec9f968882ebf6f481
PisethKhon <piseth.khon@allweb.com.kh>
2021-04-29 12:05:37 +0800

    Revert "init customer moduls"
    This reverts commit 60141f9058482707e1d06ed7e73c50ef9a6d8f36.

--- src/main/java/com/example/kafka/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kafka.repositories;
-
-import com.example.kafka.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-}
